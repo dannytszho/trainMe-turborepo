09756aefa5af8a543b518b0dd50579ff1734550d
dannytszho <dannytszho@gmail.com>
2022-05-11 18:37:02 -0700

    config trainMe-turborepo with nextJS, storybook

---
+++ .storybook/main.js
@@ -0,0 +1,12 @@
+module.exports = {
+  stories: [
+    '../apps/stories/**/*.stories.mdx',
+    '../apps/stories/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: '@storybook/react',
+}
